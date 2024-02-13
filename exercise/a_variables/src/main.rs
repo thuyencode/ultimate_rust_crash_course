4acb3df21ea8422b7cc9eef0cd89102c75021d8d
Thuyen Code <thuyencode@proton.me>
2024-02-13 22:29:33 +0700

    Complete the Exercise A

--- exercise/a_variables/src/main.rs
+++ exercise/a_variables/src/main.rs
@@ -1,3 +1,37 @@
+const STARTING_MISSILES: i32 = 8; 
+const READY_AMOUNT: i32 = 2;
+
 fn main() {
-    println!("Hello, world!");
+    part_1();
+    println!();
+    
+    part_2();
+    println!();
+
+    extra();
+}
+
+fn part_1() {
+    let missiles = STARTING_MISSILES;
+    let ready = READY_AMOUNT;
+
+    println!("Firing {} of my {} missiles...", ready, missiles);
+}
+
+fn part_2() {
+    let mut missiles = 8;
+    let ready = 2;
+
+    println!("Firing {} of my {} missiles...", ready, missiles);
+
+    missiles -= ready;
+
+    println!("{} missiles left", missiles);
+}
+
+fn extra() {
+    let (missiles, ready) = (STARTING_MISSILES, READY_AMOUNT);
+
+    println!("Firing {} of my {} missiles...", ready, missiles);
+    println!("{} missiles left", missiles - ready);
 }
